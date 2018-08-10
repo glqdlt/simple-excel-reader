d4b55b60e7f2b685583835a1f0b333ad038883af
glqdlt <dlfdnd0725@gmail.com>
2018-08-10 11:09:32 +0900

    자바 8 이기때문에 함수형 인터페이스의 선언

--- src/main/java/com/glqdlt/utill/simpleReader/callback/SimpleReaderCallBack.java
+++ src/main/java/com/glqdlt/utill/simpleReader/callback/SimpleReaderCallBack.java
@@ -2,9 +2,11 @@ package com.glqdlt.utill.simpleReader.callback;
 
 import org.apache.poi.ss.usermodel.Row;
 
+
 /**
  * @param <T>
  */
+@FunctionalInterface
 public interface SimpleReaderCallBack<T> {
     T exec(Row row);
 }
