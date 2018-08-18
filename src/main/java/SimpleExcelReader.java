14c93ab03e134778130faf13cefe10ed4bc3cafd
iw.jhun <dlfdnd0725@gmail.com>
2018-08-18 09:04:43 +0900

    자바 8, 간단한 구현

--- src/main/java/SimpleExcelReader.java
+++ src/main/java/SimpleExcelReader.java
@@ -1,8 +1,30 @@
+import callback.ReaderHandler;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import java.io.IOException;
+import java.io.InputStream;
 
-public class SimpleExcelReader  {
+public class SimpleExcelReader {
 
-    public void read(){
+    public void read(InputStream is, ReaderHandler readerHandler) {
+
+        try {
+            XSSFWorkbook sheets = new XSSFWorkbook(is);
+
+            XSSFSheet sheet = sheets.getSheetAt(0);
+
+            for (Row row : sheet) {
+                readerHandler.handler(row);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
--- src/test/java/SimpleExcelReaderTest.java
+++ src/test/java/SimpleExcelReaderTest.java
@@ -1,14 +1,30 @@
+import callback.ReaderHandler;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import static org.junit.Assert.*;
+
 
 public class SimpleExcelReaderTest {
 
     @Test
-    public void constructed() {
+    public void constructed() throws IOException {
 
+        ReaderHandler readerHandler = (row) -> {
+            System.out.println(row.getRowNum());
+            return null;
+        };
+
+
+        try(FileInputStream inputStream = new FileInputStream(new File("src/test/resources/test-data.xlsx"))){
         SimpleExcelReader simpleExcelReader = new SimpleExcelReader();
-        simpleExcelReader.read();
+            simpleExcelReader.read(inputStream, readerHandler);
+
+        }
 
     }
 }
